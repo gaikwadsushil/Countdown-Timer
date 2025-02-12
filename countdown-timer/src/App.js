80d637cdd4aff4514902a3bf14e0516021091c44
sushil gaikwad <sushilgaik2003@gmail.com>
2025-02-12 14:07:56 +0530

    first commit

---
+++ countdown-timer/src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import './App.css';
+import CountdownTimer from "./components/CountDown";
+function App() {
+  return (
+    <div className="App">
+      <CountdownTimer />
+    </div>
+  );
+}
+
+export default App;
